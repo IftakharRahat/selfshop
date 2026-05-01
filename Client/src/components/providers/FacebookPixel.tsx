b73c4f47e7b8e3f87fc7e73f4e2fd33a6495eda3
Robiul Hassan <robiulhassan993@gmail.com>
2026-05-01 20:30:26 +0600

    refactor(tracking): clean up FacebookPixel provider component
    - Remove unused import to prevent lint warnings
    - Maintain existing pixel tracking functionality

--- Client/src/components/providers/FacebookPixel.tsx
+++ Client/src/components/providers/FacebookPixel.tsx
@@ -47,7 +47,6 @@ export default function FacebookPixel() {
 						t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
 						document,'script','https://connect.facebook.net/en_US/fbevents.js');
 						fbq('init', '${pixelId}');
-						fbq('track', 'PageView');
 					`,
 				}}
 			/>
